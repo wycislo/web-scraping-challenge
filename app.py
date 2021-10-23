0b7de2b428d4f125b96c08a7a629e9b029034251
wycislo <wycislo@yahoo.com>
2021-10-23 00:09:20 -0500

    backup notebook

--- app.py
+++ app.py
@@ -4,3 +4,22 @@
 from flask import Flask, render_template, redirect
 import pymongo
 import scrape_mars
+
+
+app = Flask(__name__)
+
+# mongo connection string 
+# mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
+
+
+# Use PyMongo to establish Mongo connection
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client.mars_db
+collection = db.mars
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
