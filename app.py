a390c59f96a9ad48cc0d847d231fef6246b8c885
wycislo <wycislo@yahoo.com>
2021-10-24 03:45:53 -0500

    update

--- app.py
+++ app.py
@@ -3,41 +3,25 @@
 
 import os
 from flask import Flask, render_template, redirect
-import pymongo
 import mission_to_mars
+from flask_pymongo import PyMongo
+
 
 
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-client = pymongo.MongoClient('mongodb://localhost:27017')
-db = client.mars_db
-collection = db.images
-
-# print(client.list_database_names())
-
-# check if database exists 
-dblist = client.list_database_names()
-if collection in dblist:
-     print('Database exists')
-
-
-# collection.drop()
-# #db.dropdatabase()
-
-# mydict = {"url_name": "https://nasa.gov/mars"}
-# x = collection.insert_one(mydict)
-
+app.config["MONGO_URI"] = ('mongodb://localhost:27017/mars_db')
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    mars = db.images.find_one()
+    mars = mongo.db.images.find_one()
     return render_template("index.html",mars = mars)
 
-@app.route('/scrape')
-def scrape():
-	mission_to_mars.scrape()
-	return redirect('/', code = 302)
+# @app.route('/scrape')
+# def scrape():
+# 	mission_to_mars.scrape()
+# 	return redirect('/', code = 302)
 
 
 if __name__ == "__main__":
--- mission_to_mars.py
+++ mission_to_mars.py
@@ -1,26 +1,46 @@
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import requests
-from time import sleep
+import time
+import pymongo
+
+# set up mongo
+# Use PyMongo to establish Mongo connection
+client = pymongo.MongoClient('mongodb://localhost:27017')
+db = client.mars_db
+collection = db.images
+
+# print(client.list_database_names())
+
+# check if database exists if it does, delete it 
+dblist = client.list_database_names()
+print(dblist)
+
+if 'mars_db' in dblist:
+    print('Database exists')
+    db.drop_collection('images')
+    print('mars_db dropped')
+
+
+# news
+url = "https://mars.nasa.gov/news/"
 
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-mars_news_site = "https://mars.nasa.gov/news/"
-
-browser.visit(mars_news_site)
+# # Retrieve page with the requests module
+# response = requests.get(url)
+browser.visit(url)
 html = browser.html
-soup = bs(html,'html.parser')
 
-news_title = soup.find_all("div", class_="list_text")
-news_title[0]
+# Create BeautifulSoup object; parse with 'html.parser'
+news_soup = BeautifulSoup(html, 'html.parser')
+nasa_news = news_soup.find("div", class_="list_text")
+news_title = nasa_news.find("div", class_="content_title")
+print(news_title.get_text())
 
-title= news_title[0].find("div",class_='content_title').get_text()
-paragraph = news_title[0].find("div",class_='article_teaser_body').get_text()
-print(title)
-print(paragraph)
 
 
 # find featured image
@@ -28,16 +48,31 @@ target_site = 'https://spaceimages-mars.com'
 browser.visit(target_site)
 
 html = browser.html
-soup = bs(html,'html.parser')
+soup = BeautifulSoup(html,'html.parser')
 
 #featured_image_url = browser.find_by_partial_text('featured').value
 featured_image_url = target_site + soup.find('img', class_='headerimage fade-in')['src']
-print(featured_image_url)
+#print(featured_image_url)
+featured_image_title = target_site + soup.find('h1', class_="media_feature_title").text.strip()
+
+# mars facts
+mars_facts = "https://galaxyfacts-mars.com"
+mars_series = pd.read_html(mars_facts)
+# There are two tables on this page, read the first one
+mars_df = mars_series[1]
+mars_df.columns = ['Label', 'Mars']
+mars_df.set_index('Label', inplace=True)
+#drop index.name to avoid funky second index header row
+mars_df.index.name=None
+# create html version of pandas table
+html_table = mars_df.to_html(index=False, header=False, classes="table table-striped")
+
+
 
 hemi_url = 'https://marshemispheres.com'
 #parse the landing page, isolate the image links in the item div
 hemi_html = browser.html
-hemi_soup = bs(hemi_html, "html.parser")
+hemi_soup = BeautifulSoup(hemi_html, "html.parser")
 hemi_links = hemi_soup.find_all("div", class_ = "item")
 
 #create a list for the names and image links
@@ -62,7 +97,7 @@ for link in hemi_links:
     
     #parse the html
     indv_hemi_html = browser.html
-    indv_hemi_soup = bs(indv_hemi_html, 'html.parser')
+    indv_hemi_soup = BeautifulSoup(indv_hemi_html, 'html.parser')
     
     #grab the full image link from wide-image src
     indv_hemi_url = indv_hemi_soup.find("img", class_ = "wide-image")["src"]
@@ -76,3 +111,16 @@ for link in hemi_links:
     browser.back()
 
 browser.quit()
+
+
+#  # Return results
+mission_to_mars ={
+    'news_title' : nasa_news.get_text(),
+	'summary': news_title.get_text(),
+    'featured_image_title': featured_image_title,
+    'featured_image': featured_image_url
+    
+}
+
+
+collection.insert_one(mission_to_mars)
